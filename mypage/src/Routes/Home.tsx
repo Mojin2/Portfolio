a2509f91172dede930e14a805c8e87a06574a025
Mojin2 <aiduriaaa@gmail.com>
2023-06-09 19:55:01 +0900

    updated version1

--- mypage/src/Routes/Home.tsx
+++ mypage/src/Routes/Home.tsx
@@ -15,13 +15,49 @@ import {
 const Wrapper = styled.div`
   width: 100vw;
   height: 100vh;
-  background: ${(props) => props.theme.bgColor};
+  /* background: ${(props) => props.theme.bgColor}; */
+  background: tomato;
   transition-duration: 0.5s;
   display: flex;
   flex-direction: row;
+  position: absolute;
 `;
-const LeftWrapper = styled.div``;
-const RightWrapper = styled.div``;
+const Forest = styled.div`
+  position: absolute;
+  /* background-image: url("/images/forest2.png"); */
+  background-size: contain;
+  width: 100vw;
+  height: 100vh;
+  z-index: 0;
+  top: 200px;
+`;
+const Backgroud = styled.div`
+  position: absolute;
+  width: 100vw;
+  height: 100vh;
+  background: linear-gradient(-45deg, #1d2c41, #b7c9d9, #4fa3f0, #22131a);
+  background-repeat: no-repeat;
+  background-size: 400% 400%;
+  animation: backgroundChange 20s ease-in-out infinite;
+  z-index: -1;
+  @keyframes backgroundChange {
+    0% {
+      background-position: 0 50%;
+    }
+    90% {
+      background-position: 100% 50%;
+    }
+    100% {
+      background-position: 0 50%;
+    }
+  }
+`;
+const LeftWrapper = styled.div`
+  z-index: 1;
+`;
+const RightWrapper = styled.div`
+  z-index: 1;
+`;
 const TextWrapper = styled.div`
   display: flex;
   flex-direction: column;
@@ -31,9 +67,11 @@ const TextWrapper = styled.div`
   margin-left: 250px;
 `;
 const Text = styled.div`
-  color: ${(props) => props.theme.textColor};
+  /* color: ${(props) => props.theme.textColor}; */
+  color: white;
   font-size: 101px;
-  line-height: 0.8;
+  line-height: 0.85;
+  /* text-shadow: -4px 0 #000, 0 4px #000, 4px 0 #000, 0 -4px #000; */
 `;
 const ToggleButtonWrapper = styled.div`
   margin-top: 100px;
@@ -89,61 +127,72 @@ function Home() {
     }
   };
   return (
-    <Wrapper>
-      <LeftWrapper>
-        <TextWrapper>
-          <Text style={{ fontWeight: "500", fontSize: "80px" }}>WHAT'S</Text>
-          <Text style={{ fontWeight: "500", fontSize: "90px" }}>IN MY</Text>
-          <Text style={{ fontWeight: "700", fontSize: "100px" }}>CAREER</Text>
-          <Text style={{ fontWeight: "700", fontSize: "100px" }}>2023</Text>
-        </TextWrapper>
-        <ToggleButtonWrapper>
-          <ToggleButton Id="Circle1" />
-        </ToggleButtonWrapper>
-      </LeftWrapper>
-      <RightWrapper>
-        <DragDropContext onDragEnd={onDragEnd}>
-          <Droppable droppableId="droppable">
-            {(magic, snapshot) => (
-              <NaviWrapper
-                isDragginOver={snapshot.isDraggingOver}
-                isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
-                {...magic.droppableProps}
-                ref={magic.innerRef}
-              >
-                {navimenus.map((menu, idx) => (
-                  <Draggable key={menu} draggableId={menu} index={idx}>
-                    {(magic, snapshot) => (
-                      <NaviMenu
-                        isDragging={snapshot.isDragging}
-                        {...magic.dragHandleProps}
-                        {...magic.draggableProps}
-                        ref={magic.innerRef}
-                        bg={menu}
-                        onClick={() =>
-                          menu === "WHO AM I"
-                            ? navigate("/who")
-                            : menu === "MY WORKS"
-                            ? navigate("/works")
-                            : menu === "MY VISION"
-                            ? navigate("/vision")
-                            : menu === "MORE DETAILS"
-                            ? navigate("/more")
-                            : null
-                        }
-                      >
-                        {menu}
-                      </NaviMenu>
-                    )}
-                  </Draggable>
-                ))}
-                {magic.placeholder}
-              </NaviWrapper>
-            )}
-          </Droppable>
-        </DragDropContext>
-      </RightWrapper>
-    </Wrapper>
+    <div
+      style={{
+        position: "relative",
+        overflow: "hidden",
+        width: "100vw",
+        height: "100vh",
+      }}
+    >
+      <Wrapper>
+        <Backgroud />
+        <LeftWrapper>
+          <TextWrapper>
+            <Text style={{ fontWeight: "500", fontSize: "80px" }}>WHAT'S</Text>
+            <Text style={{ fontWeight: "500", fontSize: "90px" }}>IN MY</Text>
+            <Text style={{ fontWeight: "700", fontSize: "100px" }}>CAREER</Text>
+            <Text style={{ fontWeight: "700", fontSize: "100px" }}>2023</Text>
+          </TextWrapper>
+          {/* <ToggleButtonWrapper>
+            <ToggleButton Id="Circle1" />
+          </ToggleButtonWrapper> */}
+        </LeftWrapper>
+        <RightWrapper>
+          <DragDropContext onDragEnd={onDragEnd}>
+            <Droppable droppableId="droppable">
+              {(magic, snapshot) => (
+                <NaviWrapper
+                  isDragginOver={snapshot.isDraggingOver}
+                  isDraggingFromThis={Boolean(snapshot.draggingFromThisWith)}
+                  {...magic.droppableProps}
+                  ref={magic.innerRef}
+                >
+                  {navimenus.map((menu, idx) => (
+                    <Draggable key={menu} draggableId={menu} index={idx}>
+                      {(magic, snapshot) => (
+                        <NaviMenu
+                          isDragging={snapshot.isDragging}
+                          {...magic.dragHandleProps}
+                          {...magic.draggableProps}
+                          ref={magic.innerRef}
+                          bg={menu}
+                          onClick={() =>
+                            menu === "WHO AM I"
+                              ? navigate("/who")
+                              : menu === "MY WORKS"
+                              ? navigate("/works")
+                              : menu === "MY VISION"
+                              ? navigate("/vision")
+                              : menu === "MORE DETAILS"
+                              ? navigate("/more")
+                              : null
+                          }
+                        >
+                          {menu}
+                        </NaviMenu>
+                      )}
+                    </Draggable>
+                  ))}
+                  {magic.placeholder}
+                </NaviWrapper>
+              )}
+            </Droppable>
+          </DragDropContext>
+        </RightWrapper>
+      </Wrapper>
+      <Forest />
+    </div>
   );
 }
 
