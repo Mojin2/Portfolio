917b901cb32457219ed80776adf388b71fa62802
Mojin2 <aiduriaaa@gmail.com>
2023-07-15 20:51:51 +0900

    updated deploy version

--- mypage/src/Components/WorksDetails.tsx
+++ mypage/src/Components/WorksDetails.tsx
@@ -81,7 +81,7 @@ function WorksDetails({ id }: IdProps) {
   return (
     <Wrapper>
       <LeftWrapper>
-        <Image src={`/images/${work.id}.PNG`} />
+        <Image src={`./images/${work.id}.PNG`} />
       </LeftWrapper>
       <RightWrapper>
         <Title>{work.title}</Title>
